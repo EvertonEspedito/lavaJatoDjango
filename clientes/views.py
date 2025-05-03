969d4d569eb6f301dbb1a7de804e71399bd5765e
Everton Espedito <evertondev505@gmail.com>
2025-05-03 10:59:40 -0300

    update info cliente 03052505
    informações do formulário do cliente

--- clientes/views.py
+++ clientes/views.py
@@ -3,4 +3,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 def clientes (request):
-    return render(request, 'clientes.html')
+    if request.method == "GET":
+        return render(request, 'clientes.html')
+    elif request.method == "POST":
+        #pegar dados do cliente
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        contato = request.POST.get('contato')
+        #pegar dados do(s) veiculos do cliente
+        tipos = request.POST.getlist('tipo')
+        modelo = request.POST.getlist('modelo')
+        placa = request.POST.getlist('placa')
+        ano = request.POST.getlist('ano')
